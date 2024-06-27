b721c6fa8dd079dd1018a88f6e5f2ff7b43c2f3d
Jad-Atout <JadAtout.2003@outlook.sa>
2024-06-27 11:29:07 +0300

    *

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-var data=`<h2> this is my favorite 3 programming languages</h2>`;
-data+="<ul>";
-var list=[`C++`,`Java Sceipt`,`Python`];
-for(var i=0;i<3;i++)
-{
-    data+=`<li>${list[i]}</li>`;
+var forms = document.querySelector("form");
+var coins=
+    {
+        USD:3.5,
+        JOD:5,
+        NIS:1
+    };
+forms.onsubmit= function(e) {
+    e.preventDefault();
+    var elemnts=e.target.elements;
+    var valu=elemnts[0].value;
+    var resul=document.querySelector("p");
+    var calc=valu*coins[elemnts[1].value];
+    resul.textContent=`The converted amount is ${calc} NIS `;
+    var table=document.querySelector("table");
+    table.innerHTML+=data;
 }
-document.querySelector("div").innerHTML=data;
